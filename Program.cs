16ea209741ce7455cf7d5d423188d297c5872365
zaid564 <86477973+zaid564@users.noreply.github.com>
2021-09-21 15:26:47 +0530

    "Added: Employee Wage Calculated"

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 8;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
+    }
+}
