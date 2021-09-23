915f599f275951eabb4d49a7c5d6c8d437941531
zaid564 <86477973+zaid564@users.noreply.github.com>
2021-09-23 12:46:42 +0530

    Added"CalculatedAWagesTillATotalWorkingHrsReaches"

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UC_6LimitationOfWorkingHrs
+{
+    class Program
+    { 
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
+        static void Main(string[] args)
+        {
+            //Variable
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("days:" + totalWorkingDays + " Emp Hrs :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage:  "+totalEmpWage);
+               
+           
+            
+        }
+    }
+}
